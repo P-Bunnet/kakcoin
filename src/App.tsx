56f733cba39a50c75f2848ea70696d38f53ccab4
Bunnet <bunnetphoung123@gmail.com>
2021-09-30 17:42:32 +0700

    update css 30.09.21

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   const [search,setSearch] = useState('')
 
   useEffect(()=>{
+    
     axios.get<CurrencyData[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
       .then(res=> {
         setCoins(res.data);
@@ -25,27 +26,63 @@ function App() {
   const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
-    <div className='kak-coin'>
-      <div className='coin-search'>
+    <div className='text-gray-600  bg-blue-100 '>
+      
+      <div>
         {/* <h1 className="kakcoin">KAKCoin</h1> */}
-        <h2 className='coin-text'>Search a currency</h2>
+        <div>
+        <nav className="bg-red-400 px-4 p-4 md:bg-blue-600 text-white">
+          <div className="flex items-center justify-between">
+
+            <div className="flex items-center">
+              <div>
+                <p>KAKCOIN</p>
+              </div>
+
+            <div className='pl-5'>
+                <a href="" className='pr-4'>Home</a>
+                <a href=""  className='pr-4'>About</a>
+                <a href=""  className='pr-4'>Contact</a>
+            </div>
+        </div>
+        <div>
+          <img src="../popcat.gif" alt="logo" className='rounded-full flex items-center justify-center'/>
+        </div>
+        <div>
+            <a href="" className='pr-4'>Log in</a>
+            <a href="" className='pr-4'>Sign up</a>
+        </div>
+
+    </div>
+</nav>
+        </div> 
+        {/* // end nav */}
+
+
+        <div className='text-center pt-6 pb-6'>
+          <h2 className='coin-text'>Search a currency</h2>
         <form>
           <input
-            className='coin-input'
+            className='border-2 border-black'
             type='text'
             onChange={handleChange}
             placeholder='Search'
           />
         </form>
+        </div>
+        
         
       </div>
-      <table className="table-name">
-          <th className="coin">Coin</th>
-          <th className="price">Price</th>
-          <th className="volume">24h Volume</th>
-          <th className="24H">24H Changes</th>
-          <th className="Market-cap">Market Cap</th>
+      <div>
+      <table className='table=fixed min-w-full'>
+          <th className='w-4/12'>Coin</th>
+          <th className="w-1/8">Price</th>
+          <th className="w-1/8">24h Volume</th>
+          <th className="w-1/8">24H Changes</th>
+          <th className="w-1/8">Market Cap</th>
         </table>
+      </div>
+      <div>
       {filteredCoins.map(coin => {
         return (
           <Coin
@@ -60,6 +97,8 @@ function App() {
           />
         );
       })}
+      </div>
+      
     </div>
   );
 }
--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -16,30 +16,35 @@ const Coin = ({
     marketcap:number,
     volume:number,
     image:string,
-    priceChange:number}) => {
+    priceChange:number|undefined}) => {
     return (
-      <div className='coin-container'>
-        <div className='coin-row'>
-          <div className='coin'>
-            <img src={image} alt='crypto' />
-            <h1>{name}</h1>
-            <p className='coin-symbol'>{symbol}</p>
-          </div>
-          <div className='coin-data'>
-            <p className='coin-price'>${price}</p>
-            <p className='coin-volume'>${volume.toLocaleString()}</p>
-  
-            {priceChange < 0 ? (
-              <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
-            ) : (
-              <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
-            )}
-  
-            <p className='coin-marketcap'>
-              ${marketcap.toLocaleString()}
-            </p>
-          </div>
-        </div>
+      <div className='coin-container '>
+          <table className='table-fixed'>
+            <tr className='w-full'>
+              <td className='w-4/12'>
+                <tr>
+                <td className='pl-5'>
+                  <img src={image} alt='crypto' />
+                  </td>
+                <td className='font-bold pl-5'>{name}</td>
+                <td className='coin-symbol uppercase pl-5'>{symbol}</td>
+                </tr>
+                
+                </td>
+              
+
+              <td className='w-1/6'>${price}</td>
+              <td className='w-1/6'>${volume.toLocaleString()}</td>
+              {priceChange === undefined ? <td>Null</td>:
+              priceChange>0 ? 
+              <td className='text-green-600 w-1/6'>{priceChange.toFixed(2)}%</td>:
+              <td className='text-red-600 w-1/6'>{priceChange.toFixed(2)}%</td>}
+              <td className='w-1/6'>
+                ${marketcap.toLocaleString()}
+              </td>
+            </tr>
+            
+          </table>
       </div>
     );
   };
